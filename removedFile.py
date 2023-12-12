aa5795afcceb3cddccc26293bd63ceefd38e4739
qstion05 <qstion05@gmail.com>
2023-12-12 14:51:58 +0900

    data argumetation fix & fileRemover Create

---
+++ removedFile.py
@@ -0,0 +1,10 @@
+import os
+# 폴더 경로 설정
+# 폴더 내부의 모든 파일 목록 얻기
+all_files = os.listdir('dataset')
+print(all_files)
+
+for wav_file in all_files:
+	if "t" in wav_file or "p" in wav_file:
+		print("delete: ", wav_file)
+		os.unlink('dataset/' + wav_file)
